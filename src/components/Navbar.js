8f8b9925aa20cf97153ac7798412104c687e4615
Calfredo <catimbangcedrickalfredo@gmail.com>
2024-10-05 14:36:15 +0800

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import './Navbar.css';
 const Navbar = () => {
     return (
         <header>
-            <img className="logo" id="logo-image" src="/logo1.png" alt="Logo" />
+            <img className="logo" id="logo-image" src="/logo1.png" alt="" />
             <ul>
                 <li><a href="#banner">Home</a></li>
                 <li><a href="#prog-lang">About</a></li>
